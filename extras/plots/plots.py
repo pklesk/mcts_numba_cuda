97b5fa29cfae8e23a3e1c575b1c713d5e46bbdea
pklesk <pklesk@wi.zut.edu.pl>
2024-09-16 16:34:47 +0200

    about to perform Gomoku matchup (60 s per move)

--- extras/plots/plots.py
+++ extras/plots/plots.py
@@ -82,13 +82,34 @@ def scores_plot_ocp_thrifty_vs_vanilla():
         ])
     scores_plot_generator(experiments_hs_array, "$m$ (n_playouts)", "$T$ (n_trees)", [32, 64, 128, 256], [1, 2, 4, 8], None, "MCTS-NC OCP-THRIFTY (1s) \nvs MCTS VANILLA (4s)")    
 
+def scores_plot_ocp_prodigal_vs_vanilla():
+    experiments_hs_array = np.array([
+        ["1302514517_91136_427_[mcts_4_inf_vanilla;mctsnc_1_inf_1_32_ocp_prodigal;C4_6x7;100]",
+         "2439094103_18658_427_[mcts_4_inf_vanilla;mctsnc_1_inf_1_64_ocp_prodigal;C4_6x7;100]",
+         "0611358305_22348_427_[mcts_4_inf_vanilla;mctsnc_1_inf_1_128_ocp_prodigal;C4_6x7;100]",
+         "3982047969_49772_427_[mcts_4_inf_vanilla;mctsnc_1_inf_1_256_ocp_prodigal;C4_6x7;100]"],
+        ["1281441239_38466_427_[mcts_4_inf_vanilla;mctsnc_1_inf_2_32_ocp_prodigal;C4_6x7;100]",
+         "2418020825_65988_427_[mcts_4_inf_vanilla;mctsnc_1_inf_2_64_ocp_prodigal;C4_6x7;100]",
+         "2356598209_12844_427_[mcts_4_inf_vanilla;mctsnc_1_inf_2_128_ocp_prodigal;C4_6x7;100]",
+         "1432320577_07564_427_[mcts_4_inf_vanilla;mctsnc_1_inf_2_256_ocp_prodigal;C4_6x7;100]"],
+        ["1239294683_33126_427_[mcts_4_inf_vanilla;mctsnc_1_inf_4_32_ocp_prodigal;C4_6x7;100]",         
+         "2375874269_93352_427_[mcts_4_inf_vanilla;mctsnc_1_inf_4_64_ocp_prodigal;C4_6x7;100]",
+         "1552110721_61132_427_[mcts_4_inf_vanilla;mctsnc_1_inf_4_128_ocp_prodigal;C4_6x7;100]",
+         "0627833089_55852_427_[mcts_4_inf_vanilla;mctsnc_1_inf_4_256_ocp_prodigal;C4_6x7;100]"],
+        ["1155001571_55150_427_[mcts_4_inf_vanilla;mctsnc_1_inf_8_32_ocp_prodigal;C4_6x7;100]",
+         "2291581157_82672_427_[mcts_4_inf_vanilla;mctsnc_1_inf_8_64_ocp_prodigal;C4_6x7;100]",
+         "4238103041_90412_427_[mcts_4_inf_vanilla;mctsnc_1_inf_8_128_ocp_prodigal;C4_6x7;100]",
+         "3313825409_85132_427_[mcts_4_inf_vanilla;mctsnc_1_inf_8_256_ocp_prodigal;C4_6x7;100]"]
+        ])
+    scores_plot_generator(experiments_hs_array, "$m$ (n_playouts)", "$T$ (n_trees)", [32, 64, 128, 256], [1, 2, 4, 8], None, "MCTS-NC OCP-PRODIGAL (1s) \nvs MCTS VANILLA (4s)")
+
 def best_action_plot(steps_black, qs_black, ucbs_black, steps_white, qs_white, ucbs_white, label_qs_black, label_ucbs_black, label_qs_white, label_ucbs_white, label_x, label_y, title_1, title_2):
-    figsize = (10, 6.5)        
+    figsize = (10, 6.0)        
     fontsize_suptitle = 20
     fontsize_title = 21
-    fontsize_ticks = 16
+    fontsize_ticks = 13
     fontsize_labels = 18
-    fontsize_legend = 14
+    fontsize_legend = 13
     grid_color = (0.4, 0.4, 0.4) 
     grid_dashes = (4.0, 4.0)
     legend_loc = "best"
@@ -111,6 +132,8 @@ def best_action_plot(steps_black, qs_black, ucbs_black, steps_white, qs_white, u
     plt.legend(loc=legend_loc, prop={"size": fontsize_legend}, handlelength=legend_handlelength, labelspacing=legend_labelspacing)
     plt.gca().xaxis.set_major_locator(MultipleLocator(1))
     plt.gca().yaxis.set_major_locator(FixedLocator([0, 0.25, 0.5, 0.75, 1.0]))
+    plt.xticks(fontsize=fontsize_ticks)
+    plt.yticks(fontsize=fontsize_ticks)    
     plt.grid(color=grid_color, zorder=0, dashes=grid_dashes)  
     plt.tight_layout(pad=0.4) 
     plt.show()
@@ -138,6 +161,7 @@ def best_action_plot_generator(experiments_hs, game_index, label_qs_black, label
 
 if __name__ == "__main__":        
     # scores_plot_ocp_thrifty_vs_vanilla()
-    best_action_plot_generator("2249034921_44724_427_[mcts_4_inf_vanilla;mctsnc_1_inf_4_64_ocp_thrifty;C4_6x7;100]", 25, #11, 25, 47 
-                               "BEST $\widehat{q}$ - MCTS-NC_1_INF_4_64_OCP_THRIFTY", "UCB - MCTS-NC_1_INF_4_64_OCP_THRIFTY", 
-                               "BEST $\widehat{q}$ - MCTS_4_INF_VANILLA", "UCB - MCTS_4_INF_VANILLA", "MOVES ROUND", "BEST ACTIONS': $\widehat{q}$, UCB", None, "SAMPLE GAME OF CONNECT 4")
+    scores_plot_ocp_prodigal_vs_vanilla()
+    # best_action_plot_generator("2249034921_44724_427_[mcts_4_inf_vanilla;mctsnc_1_inf_4_64_ocp_thrifty;C4_6x7;100]", 25, #11, 25, 47 
+    #                            "BEST $\widehat{q}$ - MCTS-NC_1_INF_4_64_OCP_THRIFTY", "UCB - MCTS-NC_1_INF_4_64_OCP_THRIFTY", 
+    #                            "BEST $\widehat{q}$ - MCTS_4_INF_VANILLA", "UCB - MCTS_4_INF_VANILLA", "MOVES ROUND", "BEST ACTIONS': $\widehat{q}$, UCB", None, "SAMPLE GAME OF CONNECT 4")
