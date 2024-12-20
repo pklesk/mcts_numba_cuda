61bd1f08a90b9e496c2bb3d538718b6b549c0c27
pklesk <pklesk@wi.zut.edu.pl>
2024-12-20 13:25:34 +0100

    gomoku experiment 2094160108 registered (and plots ready)

--- src/plots.py
+++ src/plots.py
@@ -161,41 +161,7 @@ def scores_array_plot_acp_prodigal_vs_vanilla():
          "1605241382_13132_048_[mcts_5_inf_vanilla;mctsnc_1_inf_8_128_acp_prodigal;C4_6x7;100]",
          "0680963750_07852_048_[mcts_5_inf_vanilla;mctsnc_1_inf_8_256_acp_prodigal;C4_6x7;100]"]
         ])
-    scores_array_plot_generator(experiments_hs_array, "$m$ (n_playouts)", "$T$ (n_trees)", [32, 64, 128, 256], [1, 2, 4, 8], "ACP-PRODIGAL (1$\,$s) vs VANILLA (5$\,$s)")
-
-def scores_curve_plot(data, label_x, label_y, ticks_x, title_1, title_2):    
-    figsize = (6, 6)
-    fontsize_suptitle = 20
-    fontsize_title = 23
-    fontsize_ticks = 16
-    fontsize_labels = 20
-    fontsize_main = 20
-    fontsize_details = 13       
-    plt.figure(figsize=figsize)    
-    if title_1:
-        plt.suptitle(title_1, fontsize=fontsize_suptitle)
-    if title_2:
-        plt.title(title_2, fontsize=fontsize_title)
-    for i in range(data.shape[0]):
-        plt.plot(data[i])    
-    plt.xlabel(label_x, fontsize=fontsize_labels)
-    plt.ylabel(label_y, fontsize=fontsize_labels)
-    plt.xticks(ticks=np.arange(data.shape[1]), labels=ticks_x, fontsize=fontsize_ticks)     
-    plt.tight_layout(pad=0.4) 
-    plt.show()
-
-def scores_curve_plot_generator(experiments_hs_array, label_x, label_y, ticks_x, title_1, title_2):
-    print("SCORES-CURVE-PLOT GENERATOR...")    
-    data = np.zeros(experiments_hs_array.shape)
-    for i in range(experiments_hs_array.shape[0]):
-        for j in range(experiments_hs_array.shape[1]):
-            experiment_info = unzip_and_load_experiment(experiments_hs_array[i, j], FOLDER_EXPERIMENTS)
-            data[i, j] = experiment_info["stats"]["score_b_mean"]
-            steps = []
-            mean_depths = []
-            max_depths = []                    
-    print("SCORES-CURVE-PLOT GENERATOR DONE.")
-    scores_curve_plot(data, label_x, label_y, ticks_x, title_1, title_2)    
+    scores_array_plot_generator(experiments_hs_array, "$m$ (n_playouts)", "$T$ (n_trees)", [32, 64, 128, 256], [1, 2, 4, 8], "ACP-PRODIGAL (1$\,$s) vs VANILLA (5$\,$s)")    
 
 def best_action_plot(moves_rounds_black, qs_black, ucbs_black, moves_rounds_white, qs_white, ucbs_white, 
                      label_qs_black, label_ucbs_black, label_qs_white, label_ucbs_white, label_x, label_y, title_1, title_2,
@@ -203,7 +169,7 @@ def best_action_plot(moves_rounds_black, qs_black, ucbs_black, moves_rounds_whit
     figsize = (10.0, 5.0)        
     fontsize_suptitle = 20
     fontsize_title = 21
-    fontsize_ticks = 11
+    fontsize_ticks = 8.5 # 11
     fontsize_labels = 18
     fontsize_legend = 11
     grid_color = (0.4, 0.4, 0.4) 
@@ -234,7 +200,8 @@ def best_action_plot(moves_rounds_black, qs_black, ucbs_black, moves_rounds_whit
     plt.legend(loc=legend_loc, prop={"size": fontsize_legend}, handlelength=legend_handlelength, labelspacing=legend_labelspacing)
     plt.gca().xaxis.set_major_locator(MultipleLocator(1))
     plt.gca().yaxis.set_major_locator(FixedLocator(np.arange(0, 1.125, 0.125)))
-    plt.xticks(fontsize=fontsize_ticks)
+    ticks_x = np.arange(1, max(max(moves_rounds_black), max(moves_rounds_white)) + 1, 1)
+    plt.xticks(ticks_x, fontsize=fontsize_ticks)
     plt.yticks(fontsize=fontsize_ticks)    
     plt.grid(color=grid_color, zorder=0, dashes=grid_dashes)  
     plt.tight_layout(pad=0.4) 
@@ -270,7 +237,7 @@ def depths_plot(moves_rounds_black, mean_depths_black, max_depths_black, moves_r
     figsize = (10, 5.0)        
     fontsize_suptitle = 20
     fontsize_title = 21
-    fontsize_ticks = 11
+    fontsize_ticks = 8.5 # 11
     fontsize_labels = 18
     fontsize_legend = 11
     grid_color = (0.4, 0.4, 0.4) 
@@ -294,7 +261,8 @@ def depths_plot(moves_rounds_black, mean_depths_black, max_depths_black, moves_r
     plt.ylabel(label_y, fontsize=fontsize_labels)
     plt.legend(loc=legend_loc, prop={"size": fontsize_legend}, handlelength=legend_handlelength, labelspacing=legend_labelspacing)
     plt.gca().xaxis.set_major_locator(MultipleLocator(1))    
-    plt.xticks(fontsize=fontsize_ticks)
+    ticks_x = np.arange(1, max(max(moves_rounds_black), max(moves_rounds_white)) + 1, 1)
+    plt.xticks(ticks_x, fontsize=fontsize_ticks)
     plt.yticks(fontsize=fontsize_ticks)        
     plt.grid(color=grid_color, zorder=0, dashes=grid_dashes)  
     plt.tight_layout(pad=0.4) 
@@ -436,7 +404,7 @@ def averages_printout_gomoku_30s_acp_prodigal():
     averages_printout_generator(np.array([
         "3240654036_09850_048_[mcts_30_inf_vanilla;mctsnc_30_inf_4_256_acp_prodigal_16g;Gomoku_15x15;100]",
         "1304007724_29490_048_[mctsnc_30_inf_4_128_ocp_thrifty_16g;mctsnc_30_inf_4_256_acp_prodigal_16g;Gomoku_15x15;100]",
-        "0500042733_17720_427_[mctsnc_30_inf_2_128_ocp_prodigal_16g;mctsnc_30_inf_2_128_acp_prodigal_16g;Gomoku_15x15;100]",
+        "3876337002_15280_048_[mctsnc_30_inf_4_256_ocp_prodigal_16g;mctsnc_30_inf_4_256_acp_prodigal_16g;Gomoku_15x15;100]",
         "2094160108_21298_048_[mctsnc_30_inf_4_256_acp_thrifty_16g;mctsnc_30_inf_4_256_acp_prodigal_16g;Gomoku_15x15;100]"
         ]), 
         "MCTSNC(search_time_limit=30.0, search_steps_limit=inf, n_trees=4, n_playouts=256, variant='acp_prodigal', device_memory=16.0, ucb_c=2.0, seed: 0)")
@@ -451,25 +419,23 @@ if __name__ == "__main__":
     
     # scores_array_plot_acp_prodigal_vs_vanilla()
 
-    # best_action_plot_generator("3356143073_10572_427_[mcts_5_inf_vanilla;mctsnc_1_inf_4_256_ocp_prodigal;C4_6x7;100]", 61,
+    # best_action_plot_generator("0241191398_10572_048_[mcts_5_inf_vanilla;mctsnc_1_inf_4_256_ocp_prodigal;C4_6x7;100]", 9,
     #                            "BEST $\widehat{q}$ - MCTS_4_INF_VANILLA", "UCB - MCTS_4_INF_VANILLA",      
     #                            "BEST $\widehat{q}$ - MCTS-NC_1_INF_4_256_OCP_PRODIGAL", "UCB - MCTS-NC_1_INF_4_256_OCP_PRODIGAL",     
-    #                            "MOVES ROUND", "BEST ACTIONS': $\widehat{q}$, UCB", None, "SAMPLE GAME OF CONNECT 4 (NO. 61/100)")
-        
-    # depths_plot_generator("3356143073_10572_427_[mcts_5_inf_vanilla;mctsnc_1_inf_4_256_ocp_prodigal;C4_6x7;100]", 61,  
+    #                            "MOVES ROUND", "BEST ACTIONS': $\widehat{q}$, UCB", None, "SAMPLE GAME OF CONNECT 4 (NO. 9/100)")    
+    # depths_plot_generator("0241191398_10572_048_[mcts_5_inf_vanilla;mctsnc_1_inf_4_256_ocp_prodigal;C4_6x7;100]", 9,  
     #                       "MEAN DEPTHS - MCTS_4_INF_VANILLA", "MAX DEPTHS - MCTS_4_INF_VANILLA",
     #                       "MEAN DEPTHS - MCTS-NC_1_INF_4_256_OCP_PRODIGAL", "MAX DEPTHS - MCTS-NC_1_INF_4_256_OCP_PRODIGAL",                                  
-    #                       "MOVES ROUND", "MEAN, MAXIMUM DEPTHS", None, "SAMPLE GAME OF CONNECT 4 (NO. 61/100)")
+    #                       "MOVES ROUND", "MEAN, MAXIMUM DEPTHS", None, "SAMPLE GAME OF CONNECT 4 (NO. 9/100)")
     
-    # best_action_plot_generator("2094160108_21298_048_[mctsnc_30_inf_4_256_acp_thrifty_16g;mctsnc_30_inf_4_256_acp_prodigal_16g;Gomoku_15x15;100]", 51,
+    # best_action_plot_generator("2094160108_21298_048_[mctsnc_30_inf_4_256_acp_thrifty_16g;mctsnc_30_inf_4_256_acp_prodigal_16g;Gomoku_15x15;100]", 11,
     #                            "BEST $\widehat{q}$ - MCTS-NC_30_INF_4_256_ACP_THRIFTY", "25 x UCB - MCTS-NC_30_INF_4_256_ACP_THRIFTY",
     #                            "BEST $\widehat{q}$ - MCTS-NC_30_INF_4_256_ACP_PRODIGAL", "25 x UCB - MCTS-NC_30_INF_4_256_ACP_PRODIGAL",                                  
-    #                            "MOVES ROUND", "BEST ACTIONS': $\widehat{q}$, UCB", None, "SAMPLE GAME OF GOMOKU (NO. 51/100)", 25.0)
-    
-    # depths_plot_generator("2094160108_21298_048_[mctsnc_30_inf_4_256_acp_thrifty_16g;mctsnc_30_inf_4_256_acp_prodigal_16g;Gomoku_15x15;100]", 51,  
+    #                            "MOVES ROUND", "BEST ACTIONS': $\widehat{q}$, UCB", None, "SAMPLE GAME OF GOMOKU (NO. 11/100)", 25.0)    
+    # depths_plot_generator("2094160108_21298_048_[mctsnc_30_inf_4_256_acp_thrifty_16g;mctsnc_30_inf_4_256_acp_prodigal_16g;Gomoku_15x15;100]", 11,  
     #                       "MEAN DEPTHS - MCTS-NC_30_INF_4_256_ACP_THRIFTY", "MAX DEPTHS - MCTS-NC_30_INF_4_256_ACP_THRIFTY",
     #                       "MEAN DEPTHS - MCTS-NC_30_INF_4_256_ACP_PRODIGAL", "MAX DEPTHS - MCTS-NC_30_INF_4_256_ACP_PRODIGAL",                                  
-    #                       "MOVES ROUND", "MEAN, MAXIMUM DEPTHS", None, "SAMPLE GAME OF GOMOKU (NO. 51/100)")
+    #                       "MOVES ROUND", "MEAN, MAXIMUM DEPTHS", None, "SAMPLE GAME OF GOMOKU (NO. 11/100)")
         
     # averages_printout_c4_5s_vanilla()
     
@@ -479,7 +445,7 @@ if __name__ == "__main__":
     
     # averages_printout_c4_5s_acp_thrifty()
     
-    averages_printout_c4_5s_acp_prodigal()
+    # averages_printout_c4_5s_acp_prodigal()
     
     # averages_printout_gomoku_30s_vanilla()
     
@@ -489,24 +455,4 @@ if __name__ == "__main__":
     
     # averages_printout_gomoku_30s_acp_thrifty()
     
-    # averages_printout_gomoku_30s_acp_prodigal()
-            
-    # experiments_hs_array = np.array([
-    #     ["1302514517_91136_427_[mcts_4_inf_vanilla;mctsnc_1_inf_1_32_ocp_prodigal;C4_6x7;100]",
-    #      "2439094103_18658_427_[mcts_4_inf_vanilla;mctsnc_1_inf_1_64_ocp_prodigal;C4_6x7;100]",
-    #      "0611358305_22348_427_[mcts_4_inf_vanilla;mctsnc_1_inf_1_128_ocp_prodigal;C4_6x7;100]",
-    #      "3982047969_49772_427_[mcts_4_inf_vanilla;mctsnc_1_inf_1_256_ocp_prodigal;C4_6x7;100]"],
-    #     ["1281441239_38466_427_[mcts_4_inf_vanilla;mctsnc_1_inf_2_32_ocp_prodigal;C4_6x7;100]",
-    #      "2418020825_65988_427_[mcts_4_inf_vanilla;mctsnc_1_inf_2_64_ocp_prodigal;C4_6x7;100]",
-    #      "2356598209_12844_427_[mcts_4_inf_vanilla;mctsnc_1_inf_2_128_ocp_prodigal;C4_6x7;100]",
-    #      "1432320577_07564_427_[mcts_4_inf_vanilla;mctsnc_1_inf_2_256_ocp_prodigal;C4_6x7;100]"],
-    #     ["1239294683_33126_427_[mcts_4_inf_vanilla;mctsnc_1_inf_4_32_ocp_prodigal;C4_6x7;100]",         
-    #      "2375874269_93352_427_[mcts_4_inf_vanilla;mctsnc_1_inf_4_64_ocp_prodigal;C4_6x7;100]",
-    #      "1552110721_61132_427_[mcts_4_inf_vanilla;mctsnc_1_inf_4_128_ocp_prodigal;C4_6x7;100]",
-    #      "0627833089_55852_427_[mcts_4_inf_vanilla;mctsnc_1_inf_4_256_ocp_prodigal;C4_6x7;100]"],
-    #     ["1155001571_55150_427_[mcts_4_inf_vanilla;mctsnc_1_inf_8_32_ocp_prodigal;C4_6x7;100]",
-    #      "2291581157_82672_427_[mcts_4_inf_vanilla;mctsnc_1_inf_8_64_ocp_prodigal;C4_6x7;100]",
-    #      "4238103041_90412_427_[mcts_4_inf_vanilla;mctsnc_1_inf_8_128_ocp_prodigal;C4_6x7;100]",
-    #      "3313825409_85132_427_[mcts_4_inf_vanilla;mctsnc_1_inf_8_256_ocp_prodigal;C4_6x7;100]"]
-    #     ])
-    # scores_curve_plot_generator(experiments_hs_array, "TIME LIMIT [s]", "AVERAGE SCORES", [1/4, 1/2, 2, 4], "MCTS-NC OCP-PRODIGAL (1s) \nvs MCTS VANILLA (4s)", "TITLE 2")
+    averages_printout_gomoku_30s_acp_prodigal()        
